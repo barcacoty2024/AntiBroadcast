f2a73c8083c6c1f312acc2e23cc9b0a931de5bf4
MaxGuns <153924950+MaxGuns@users.noreply.github.com>
2023-12-24 00:48:34 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,31 @@
-# bot.py
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+from telegram import ParseMode
 
 # Ganti TOKEN_BOT dengan token yang diberikan oleh BotFather
-TOKEN_BOT = '6551801424:AAEvoJmcTvxbEoVx6_RdfuokyUBrd7qUFS8'
+TOKEN_BOT = 'your_bot_token'
 
-# Ganti CHAT_ID dengan ID grup Anda
-CHAT_ID = -1002044156039
+# Ganti CHAT_ID_OWNER dengan ID Telegram Anda
+CHAT_ID_OWNER = your_chat_id
+
+def start(update, context):
+    update.message.reply_text("Bot is active!")
 
 def anti_broadcast_handler(update, context):
-    if update.message and update.message.chat_id == CHAT_ID:
+    if update.message and update.message.chat_id == CHAT_ID_OWNER:
+        # Pemilik bot dapat melihat pesan di grup
+        print(f"Owner Message: {update.message.text}")
+    elif update.message and update.message.chat_id != CHAT_ID_OWNER:
+        # Non-pemilik bot, blokir pesan global atau broadcast
         update.message.delete()
 
 def main():
     updater = Updater(token=TOKEN_BOT, use_context=True)
     dp = updater.dispatcher
 
+    # Command '/start' untuk memastikan bot aktif
+    dp.add_handler(CommandHandler("start", start))
+
+    # Fungsi untuk menangani pesan global atau broadcast
     dp.add_handler(MessageHandler(Filters.TEXT & ~Filters.FORWARD, anti_broadcast_handler))
 
     updater.start_polling()
