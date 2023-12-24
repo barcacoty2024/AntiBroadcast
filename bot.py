a24c265aa89b15b1593647063d681abb33805b43
MaxGuns <153924950+MaxGuns@users.noreply.github.com>
2023-12-24 10:41:58 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,6 +34,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0", port=port, url_path=TOKEN_BOT)
     updater.bot.setWebhook(f"https://antip.herokuapp.com/{TOKEN_BOT}")
 
+
     # Keep the program running
     updater.idle()
 
