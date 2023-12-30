d4f9b2e054577687a0fe0dbfc2d6591b7291aec9
MaxGuns <153924950+MaxGuns@users.noreply.github.com>
2023-12-30 17:30:32 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
-from telegram import Update
-from telegram.ext import Updater, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
+from telegram import Update, ChatMemberUpdated
+from telegram.ext import Updater, MessageHandler, Filters, CallbackContext, ChatMemberHandler
 
 # Dictionary untuk menyimpan data perubahan nama dan username
 changes_data = {}
@@ -7,36 +7,28 @@ changes_data = {}
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("Selamat datang di Bot Pemantau Perubahan Nama dan Username!")
 
-def handle_message(update: Update, context: CallbackContext) -> None:
-    user = update.effective_user  # Menggunakan effective_user untuk mendapatkan informasi pengguna
-    chat_id = update.message.chat_id if update.message else None  # Memastikan bahwa update.message tidak None
+def handle_chat_member_updated(update: Update, context: CallbackContext) -> None:
+    new_chat_member = update.message.new_chat_members[0] if update.message.new_chat_members else None
+    old_chat_member = update.message.old_chat_members[0] if update.message.old_chat_members else None
 
-    # Periksa apakah user atau update.message bernilai None
-    if user and chat_id:
-        # Periksa apakah ada perubahan nama atau username
-        if user.username != changes_data.get(chat_id, {}).get(user.id, {}).get('username'):
-            context.bot.send_message(chat_id, f"🔄 {user.username} mengganti username menjadi {user.username}!")
+    # Periksa apakah ada perubahan nama atau username
+    if new_chat_member and old_chat_member and new_chat_member.username != old_chat_member.username:
+        context.bot.send_message(update.message.chat_id, f"🔄 {new_chat_member.username} mengganti username menjadi {new_chat_member.username}!")
 
-        if user.full_name != changes_data.get(chat_id, {}).get(user.id, {}).get('full_name'):
-            context.bot.send_message(chat_id, f"🔄 {user.username} mengganti nama menjadi {user.full_name}!")
-
-        # Perbarui data perubahan nama dan username
-        if chat_id not in changes_data:
-            changes_data[chat_id] = {}
-        changes_data[chat_id][user.id] = {'username': user.username, 'full_name': user.full_name}
-
+    if new_chat_member and old_chat_member and new_chat_member.full_name != old_chat_member.full_name:
+        context.bot.send_message(update.message.chat_id, f"🔄 {new_chat_member.username} mengganti nama menjadi {new_chat_member.full_name}!")
 
 def main() -> None:
     # Ganti TOKEN_BOT dengan token bot yang Anda dapatkan dari BotFather
-    updater = Updater("6551801424:AAEvoJmcTvxbEoVx6_RdfuokyUBrd7qUFS8")
+    updater = Updater("TOKEN_BOT")
 
     dp = updater.dispatcher
 
     # Menangani perintah /start
     dp.add_handler(MessageHandler(Filters.command & Filters.regex('^start$'), start))
 
-    # Menangani setiap pesan di grup
-    dp.add_handler(MessageHandler(Filters.chat_type.groups, handle_message))
+    # Menangani perubahan member chat (nama dan username)
+    dp.add_handler(ChatMemberHandler(handle_chat_member_updated, ChatMemberUpdated))
 
     # Memulai bot
     updater.start_polling()
